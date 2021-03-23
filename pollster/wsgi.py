700ff918ec6c4a15e50ebdc441db998af16633f8
Sayali Kanase <sayalikanase1910@gmail.com>
2021-03-23 16:04:33 +0530

    4th commit

--- pollster/wsgi.py
+++ pollster/wsgi.py
@@ -1,8 +1,8 @@
 
 import os
-from whitenoise.django import DjangoWhiteNoise
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollster.settings')
 
-application = DjangoWhiteNoise(application)
+application = get_wsgi_application()
